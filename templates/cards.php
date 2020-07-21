08e67f670faf33f45c04a4e5953e08a989b8b125
allanrivers <allanrivers99@gmail.com>
2020-07-21 10:01:20 -0700

    new

--- templates/cards.php
+++ templates/cards.php
@@ -3,21 +3,21 @@
     <div class="translateCards">
         <div class="red">
             <div class="card-footer">
-                <button>Red</button>
+                <button style="font-size: 1.2rem;">Red</button>
             </div>
         </div>
     
     
         <div class="green">
             <div class="card-footer">
-                <button>Green</button>
+                <button style="font-size: 1.2rem;">Green</button>
             </div>
         </div>
     
     
         <div class="blue">
             <div class="card-footer">
-                <button>Blue</button>
+                <button style="font-size: 1.2rem;">Blue</button>
             </div>
         </div>
     </div>
--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,7 @@
     <!-- CSS link -->
     <link rel="stylesheet" href="style.css">
 
+
     <!-- JQUERY CDN -->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <!-- My JS file -->
