3ae67110076f8691426504870d218c446a35c504
allanrivers <allanrivers99@gmail.com>
2020-08-02 09:49:25 -0700

    alert

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(function() {
 
-/*
+
 $(window).on('load', () => {
      alert('Unfortunately, MySQL is not supported with the hosting that I am currently using. However, it\'s fully functional on my apache server.\n\nThank you,\n-Allan Rivers');
  })
-*/
+
 
 $('#newsletterForm').on('submit', (e) => {
     e.preventDefualt();
