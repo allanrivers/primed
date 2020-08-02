ce537379e100fdbaae1de9a268478adcf006ce2a
allanrivers <allanrivers99@gmail.com>
2020-08-02 09:48:19 -0700

    responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function() {
 
 /*
-// $(window).on('load', () => {
-//     alert('Unfortunately, MySQL is not supported with the hosting that I am currently using. However, it\'s fully functional on my apache server.\n\nThank you,\n-Allan Rivers')
-// })
+$(window).on('load', () => {
+     alert('Unfortunately, MySQL is not supported with the hosting that I am currently using. However, it\'s fully functional on my apache server.\n\nThank you,\n-Allan Rivers');
+ })
 */
 
 $('#newsletterForm').on('submit', (e) => {
